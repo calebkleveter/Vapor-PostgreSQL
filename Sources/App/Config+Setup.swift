5729d8ef676e1648035c96a204de5418850a2638
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-06-01 09:07:17 -0500

    Added User preparations to Config

--- Sources/App/Config+Setup.swift
+++ Sources/App/Config+Setup.swift
@@ -1,4 +1,5 @@
 import LeafProvider
+import PostgreSQLProvider
 
 extension Config {
     public func setup() throws {
@@ -7,10 +8,16 @@ extension Config {
         Node.fuzzy = [JSON.self, Node.self]
 
         try setupProviders()
+        addPreparations()
     }
     
     /// Configure providers
     private func setupProviders() throws {
         try addProvider(LeafProvider.Provider.self)
+        try addProvider(PostgreSQLProvider.Provider.self)
+    }
+    
+    private func addPreparations() {
+        preparations.append(User.self)
     }
 }
